192292881bb7d7e558ae894421edbb14171cb385
José Brito <josealbritose@gmail.com>
2024-10-27 17:09:46 -0300

    Hero Section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from './shared/Hero/Banner'
+import Pricing from './shared/Hero/Pricing'
+
+export default function Hero() {
+  return (
+    <section className='pt-4'>
+      <Banner/>
+      <Pricing/>
+    </section>
+  )
+}
