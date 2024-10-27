7506ef6979f5f2308e578cb27d5ec54eee476cff
José Brito <josealbritose@gmail.com>
2024-10-27 17:09:52 -0300

    Banner

---
+++ src/components/shared/Hero/Banner.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Banner() {
+  return (
+    <article className="px-[20px] flex flex-col items-center justify-center ">
+      <h1 className="text-center text-4xl font-medium text-gray-800 ">
+        Start monitoring your website like a pro
+      </h1>
+      <p className="text-wrap text-start pt-6 text-gray-50 font-medium">
+        Get a bird&apos;s eye view with our customizable dashboard. <br />
+        Stray on top of things! Revamp your work process with our game-changing
+        feature. <br />
+        Boost productivity and efficiency!
+      </p>
+    </article>
+  );
+}
