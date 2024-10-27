402fa6a0d650de48c685102b4cb9dfc639eee8ca
José Brito <josealbritose@gmail.com>
2024-10-27 17:34:09 -0300

    responsive styles

--- src/components/shared/Hero/Pricing.tsx
+++ src/components/shared/Hero/Pricing.tsx
@@ -1,26 +1,20 @@
 import React from "react";
 import PricingButtons from "./PricingButtons";
-import ImageComponent from "../Navbar/ImageComponent";
 
 export default function Pricing() {
   return (
-    <aside className="flex justify-center items-center py-10 gap-x-5">
-      <div className="w-1/2 flex justify-end items-center">
+    <aside className="flex flex-col gap-y-5 md:flex-row justify-center items-center py-10 gap-x-5">
+      <div className="w-full md:w-1/2 flex  justify-center md:justify-end items-center">
         <PricingButtons
           label="Try for free"
           className="bg-[#4328eb] w-1/2 py-6 px-8 text-white rounded-lg hover:bg-[#513ecc] "
         />
       </div>
-      <div className="flex justify-start items-center w-1/2">
+      <div className="flex justify-center md:justify-start items-center w-full md:w-1/2">
         <PricingButtons
           label="View Pricing"
           className="w-1/2 text-[#523ed8] transition-colors duration-75 ease-in hover:bg-gray-10 font-semibold"
-        />
-        <ImageComponent
-          src="/assets/blue-button.svg"
-          alt="Pricing"
-          height={40}
-          width={40}
+          icon
         />
       </div>
     </aside>
--- src/components/shared/Hero/PricingButtons.tsx
+++ src/components/shared/Hero/PricingButtons.tsx
@@ -1,19 +1,32 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import React from "react";
+import ImageComponent from "../Navbar/ImageComponent";
 
 interface PricingButtonsProps {
   label: string;
   className: string;
+  icon?: true;
 }
 
 export default function PricingButtons({
   label,
   className,
+  icon,
 }: PricingButtonsProps) {
   return (
-    <Button className={`${className}`} variant='menu'>
-      {label}
-    </Button>
+    <>
+      <Button className={`${className} ${icon && 'flex justify-between items-center py-4 hover:bg-sky-50'}`} variant="menu">
+        {label}
+        {icon && (
+          <ImageComponent
+            src="/assets/blue-button.svg"
+            alt="Pricing"
+            height={40}
+            width={40}
+          />
+        )}
+      </Button>
+    </>
   );
 }
