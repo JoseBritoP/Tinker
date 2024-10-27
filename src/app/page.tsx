556ca2f65cd1cbcda547f2f8790d704900e726cc
José Brito <josealbritose@gmail.com>
2024-10-27 17:10:07 -0300

    Hero rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Navbar from '@/components/Navbar'
-import React from 'react'
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import React from "react";
 
 export default function HomePage() {
   return (
     <>
-      <Navbar/>
+      <Navbar />
+      <Hero />
     </>
-  )
+  );
 }
