a3b6bbbd5fe9d12fbc0bbf7aa2b6ea52378cc92d
José Brito <josealbritose@gmail.com>
2024-10-28 02:33:54 -0300

    HeroComponent

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from './shared/Hero/Banner'
 import Pricing from './shared/Hero/Pricing'
+import ShowDemo from './shared/Hero/ShowDemo'
 
 export default function Hero() {
   return (
     <section className='pt-4'>
       <Banner/>
       <Pricing/>
+      <ShowDemo/>
     </section>
   )
 }
