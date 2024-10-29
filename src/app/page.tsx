3662782cb44f1de1b09ebad960c90c21b96598f3
José Brito <josealbritose@gmail.com>
2024-10-29 08:51:43 -0300

    PlansRendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import FaQ from "@/components/FaQ";
 import Features from "@/components/Features";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Plans from "@/components/Plans";
 import React from "react";
 
 export default function HomePage() {
@@ -9,8 +10,11 @@ export default function HomePage() {
     <>
       <Navbar />
       <Hero />
-      <Features/>
-      <FaQ/>
+      <div className="px-[20px] lg:container lg:px-20 mx-auto">
+        <Features />
+        <FaQ />
+        <Plans />
+      </div>
     </>
   );
 }
