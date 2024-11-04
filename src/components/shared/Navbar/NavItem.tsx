a22725a5d40aed36b846e1f7448fe743b92a49a4
José Brito <josealbritose@gmail.com>
2024-11-04 16:25:58 -0300

    li remplace link element

--- src/components/shared/Navbar/NavItem.tsx
+++ src/components/shared/Navbar/NavItem.tsx
@@ -1,12 +1,12 @@
 import { navbarLinks } from "@/constants";
-import Link from "next/link";
 import React from "react";
 
 export default function NavItem() {
+  // li remplace Link element
   return (
     <ul className="hidden lg:flex justify-evenly items-center gap-x-10">
       {navbarLinks.map((link, i) => (
-        <Link className="text-gray-600 font-semibold border-b-2 border-gray-10 hover:border-b-sky-600 transition-colors duration-150 ease-out " key={i} href={link.href}>{link.label}</Link>
+        <li className="text-gray-600 font-semibold border-b-2 border-gray-10 hover:border-b-sky-600 transition-colors duration-150 ease-out cursor-pointer " key={i}>{link.label}</li>
       ))}
     </ul>
   );
